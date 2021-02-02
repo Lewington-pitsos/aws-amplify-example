630007dab9e9c39b9c0b96c94e168f0b3b2a76f1
Louka Ewington Pitsos <lewingtonpitsos@gmail.com>
2021-02-02 17:18:18 +1100

    actually set up authentication

---
+++ app/src/main/java/com/example/amplifytest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.amplifytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.Callback;
+import com.amazonaws.mobile.client.UserStateDetails;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
